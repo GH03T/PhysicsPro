0ded2a40ac83ccaac9b4dc74f95f8df707345467
GH03T <cpagnini6@gmail.com>
2025-02-18 19:27:29 -0500

    Added while loop to repeat main selection.

--- Main.py
+++ Main.py
@@ -1,55 +1,67 @@
 from FormulaList import *
 
-#Menu:
-print("Option 1: Potential/Kinetic Energy")
-print("Option 2: Calculate Work Done")
-print("Option 3: Calculate Max Turn Speed")
-selection = str(input("Select Option #: "))
+#Loop:
+Continue = True
+while(Continue == True):
+    #Menu:
+    print("Option 1: Potential/Kinetic Energy")
+    print("Option 2: Calculate Work Done")
+    print("Option 3: Calculate Max Turn Speed")
+    print("Or, Press 'Q' to Quit.")
+    selection = str(input("Select Option: "))
 
-#Potential/Kinetic Energy
-if(selection == "1"):
-    #Variables:
+    #Potential/Kinetic Energy
+    if(selection == "1"):
+        #Variables:
+        print()
+        mass = float(input("Enter Mass: "))
+        velocity = float(input("Enter Velocity: "))
+        height = float(input("Enter Height: "))
+        print()
+
+        #Calculations:
+        PotentialEnergyCalc = float(PotentialEnergy(mass, height))
+        KineticEnergyCalc = float(KineticEnergy(mass, velocity))
+
+        #Print Calculations To CLI:
+        print("Potential Energy = " + str(PotentialEnergyCalc) + " Joules")
+        print("Kinetic Energy = " + str(KineticEnergyCalc) + " Joules")
+
+    #Calculate Work Done
+    if(selection == "2"):
+        #Variables
+        print()
+        force = float(input("Enter Force: "))
+        displacement = float(input("Enter Displacement: "))
+        angle = float(input("Enter Angle: "))
+        print()
+
+        #Calculation:
+        WorkCalc = float(Work(force, displacement, angle))
+
+        #Print Calculation To CLI:
+        print("Work = " + str(WorkCalc) + " Joules")
+
+    #Calculate Max Turn Speed
+    if(selection == "3"):
+        #Variables:
+        print()
+        frictionCo = float(input("Enter Coefficent of Friction: "))
+        radius = float(input("Enter Radius: "))
+        print()
+
+        #Calculation:
+        MaxTurnSpeedCalc = float(MaxSpeedCurve(frictionCo, radius))
+        MaxTurnSpeedCalcMPH = float(MaxTurnSpeedCalc * 2.23694)
+
+        #Print Calculation To CLI:
+        print("Max Turn Speed = " + str(MaxTurnSpeedCalc) + " m/s")
+        print("Max Turn Speed = " + str(MaxTurnSpeedCalcMPH) + " Mi/h")
+
+    #Quit
+    if(selection == "Q" or selection == "q"):
+        Continue = False
+        break
+
+    #Space
     print()
-    mass = float(input("Enter Mass: "))
-    velocity = float(input("Enter Velocity: "))
-    height = float(input("Enter Height: "))
-    print()
-
-    #Calculations:
-    PotentialEnergyCalc = float(PotentialEnergy(mass, height))
-    KineticEnergyCalc = float(KineticEnergy(mass, velocity))
-
-    #Print Calculations To CLI:
-    print("Potential Energy = " + str(PotentialEnergyCalc) + " Joules")
-    print("Kinetic Energy = " + str(KineticEnergyCalc) + " Joules")
-
-#Calculate Work Done
-if(selection == "2"):
-    #Variables
-    print()
-    force = float(input("Enter Force: "))
-    displacement = float(input("Enter Displacement: "))
-    angle = float(input("Enter Angle: "))
-    print()
-
-    #Calculation:
-    WorkCalc = float(Work(force, displacement, angle))
-
-    #Print Calculation To CLI:
-    print("Work = " + str(WorkCalc) + " Joules")
-
-#Calculate Max Turn Speed
-if(selection == "3"):
-    #Variables:
-    print()
-    frictionCo = float(input("Enter Coefficent of Friction: "))
-    radius = float(input("Enter Radius: "))
-    print()
-
-    #Calculation:
-    MaxTurnSpeedCalc = float(MaxSpeedCurve(frictionCo, radius))
-    MaxTurnSpeedCalcMPH = float(MaxTurnSpeedCalc * 2.23694)
-
-    #Print Calculation To CLI:
-    print("Max Turn Speed = " + str(MaxTurnSpeedCalc) + " m/s")
-    print("Max Turn Speed = " + str(MaxTurnSpeedCalcMPH) + " Mi/h")
